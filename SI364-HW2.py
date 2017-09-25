41857ac524cb0eba305ea05c438b3f7455746a56
Julia Locke <juliaslocke@gmail.com>
2017-09-24 21:51:35 -0400

    submitting hw2

--- SI364-HW2.py
+++ SI364-HW2.py
@@ -10,13 +10,81 @@
 ## Edit the following Flask application code so that if you run the application locally and got to the URL http://localhost:5000/question, you see a form that asks you to enter your favorite number. Once you enter a number and submit it to the form, you should then see a web page that says "Double your favorite number is <number>". For example, if you enter 2 into the form, you should then see a page that says "Double your favorite number is 4". Careful about types in your Python code!
 ## You can assume a user will always enter a number only.
 
-from flask import Flask
+import requests
+import json
+from flask import Flask, request, render_template
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
 def hello_to_you():
     return 'Hello!'
+
+@app.route('/question', methods= ['POST','GET'])
+def getting_number():
+	j = """<!DOCTYPE html>
+	<html>
+	<body>
+	<form action="http://localhost:5000/result" method="GET">
+	Enter your favorite number:<br>
+	<input type="text" name="number" value="0">
+	<br>
+	<input type="submit" value="Submit">
+	</form> 
+	</body>
+	</html>""" 
+	return j
+
+@app.route('/result', methods= ['POST','GET'])
+def doubling_number():
+	if request.method == 'GET':
+		result = request.args
+		num = result.get('number')
+		new_number = 2*(int(num))
+		return "Double your favorite number is {}".format(new_number)
+
+# QUESTION 2:
+
+@app.route('/songsearch', methods= ['POST','GET'])
+def enter_song():
+	jl = """<!DOCTYPE html>
+	<html>
+	<body>
+	<form action="http://localhost:5000/songresults" method="GET">
+	Who is your favorite artist?<br>
+	<input type="text" name="artist" value="Taylor Swift">
+	<br>
+	How many song suggestions would you like?<br>
+	<input type="radio" name="number" value="1">1<br>
+	<input type="radio" name="number" value="2">2<br>
+	<input type="radio" name="number" value="3">3<br>
+	<input type="radio" name="number" value="4">4<br>
+	<input type="radio" name="number" value="5">5<br>
+	<input type="submit" value="Submit">
+	</form> 
+	</body>
+	</html>""" 
+	return jl
+
+@app.route('/songresults', methods= ['POST','GET'])
+def song_results():
+	if request.method == 'GET':
+		result = request.args
+		artist = result['artist']
+		num = int(result['number'])
+		d = {'term': artist, 'media': 'music', 'format':'json'}
+		resp = requests.get('https://itunes.apple.com/search?', params = d)
+		d_info = json.loads(resp.text)
+		count = 0
+		songs_d = {}
+		for song in d_info['results']:
+			if count >= num:
+				break
+			elif count < num:
+				songs_d[count] = song['trackName']
+				count += 1
+		return render_template("HW2results.html", result=songs_d)
+
 
 
 if __name__ == '__main__':
